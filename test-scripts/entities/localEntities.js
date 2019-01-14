f551591a2a27e725c4ed6f78dae88f9f60d600e1
Dante Ruiz <danteruiz102@gmail.com>
2019-01-14 11:16:46 -0800

    update local entities scripts

--- test-scripts/entities/localEntities.js
+++ test-scripts/entities/localEntities.js
@@ -9,13 +9,21 @@
     }, "local");
 
 
-    MyAvatar.sessionUUIDChanged.connect(function(sessionID) {
-        Entities.editEntity(cubeEntity, {
-            parentID: sessionUUID
-        });
-    });
+    var zoneEntity = Entities.addEntity({
+        type: "Zone",
+        name: "localZoneEntity",
+        parentID: MyAvatar.sessionUUID,
+        localPosition: { x: 0, y: 0, z: 0 },
+        dimensions: { x: 1.0, y: 1.0, z: 1.0 },
+        position: MyAvatar.position,
+        skyboxMode: "enabled",
+        skybox: {
+            color: "red"
+        },
+    }, "local");
 
     Script.scriptEnding.connect(function() {
         Entities.deleteEntity(cubeEntity);
+        Entities.deleteEntity(zoneEntity);
     });
 }());
