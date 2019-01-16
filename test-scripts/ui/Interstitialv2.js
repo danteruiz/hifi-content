33d6b019a74df0115af036b0ace9333b3d492603
Dante Ruiz <danteruiz102@gmail.com>
2019-01-16 13:44:52 -0800

    add image

--- test-scripts/ui/Interstitialv2.js
+++ test-scripts/ui/Interstitialv2.js
@@ -2,13 +2,32 @@
     var sphereEntity = Entities.addEntity({
         type: "Model",
         dimensions: { x: 3, y: 3, z: 3 },
-        parentID: MyAvatar.sessionUUID,
         position: MyAvatar.position,
         modelURL: Script.resourcesPath() + "/meshes/invertedSphere.fbx"
     }, "local");
 
 
+    var materialEntity = Entities.addEntity({
+        type: "Material",
+        parentID: sphereEntity,
+        materialURl: "materialData",
+        priority: 1,
+        materialData: JSON.stringify({
+            materialVersion: 1,
+            materials: {
+                "model": "hifi_pbr",
+                "albedoMap": Script.resolvePath("../../textures/interstitial.jpg"),
+                "emissiveMap": Script.resolvePath("../../textures/interstitial.jpg")
+            }
+        })
+    }, "local");
+
+
+    print(Script.resolvePath("../../textures/interstitial.jpg"));
+
+
     Script.scriptEnding.connect(function() {
         Entities.deleteEntity(sphereEntity);
+        Entities.deleteEntity(materialEntity);
     });
 }());
