babe7b8f24cf7650ffb1e70704b804f9df489328
Dante Ruiz <danteruiz102@gmail.com>
2019-03-15 14:37:26 -0700

    updating script

--- entity-scripts/kick.js
+++ entity-scripts/kick.js
@@ -4,7 +4,6 @@
         var avatars =  AvatarList.getAvatarIdentifiers();
 
         Users.kick(avatars[Math.floor(Math.random()*avatars.length)]);
-        print("testing");
     }
 
     Script.update.connect(kickRandomUser);
